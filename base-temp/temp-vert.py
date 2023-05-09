76d897e879676f1116fb51bb20fd78ba34e307f1
icarus <cesardedios94@gmail.com>
2023-05-08 23:31:14 -0400

    finished

--- base-temp/temp-vert.py
+++ base-temp/temp-vert.py
@@ -1,21 +1,22 @@
 temp_scale = ""
-
-def converter(temp_scale, temp):
-    if temp_scale == "C":
-        return temp * 9/5 + 32
-    elif temp_scale == "F":
-        return (temp - 32) * 5/9
-    elif temp_scale == "q":
-        return "Exiting..."
-    else:
-        return "Invalid scale"
-
+temp = 0.0
 while temp_scale != "q":
     # Select to convert from C to F or F to C
     temp_scale = input("Enter a temperature scale to convert from\n(C or F or q to exit): ")
+    if temp_scale == "q":
+        print("Exiting...")
+        break
+    else:
+        # Ask for temperature value
+        temp = float(input("Enter a temperature: \n"))
 
-    # Ask for temperature value
-    temp = float(input("Enter a temperature: \n"))
-
-    # Convert from C to F
-    print(converter(temp_scale, temp))
+        # Convert from C to F or F to C
+        if temp_scale == "C":
+            print(temp * 9/5 + 32)
+        elif temp_scale == "F":
+            print((temp - 32) * 5/9)
+        elif temp_scale == "q":
+            print("Exiting...")
+            break
+        else:
+            print("Invalid scale")
