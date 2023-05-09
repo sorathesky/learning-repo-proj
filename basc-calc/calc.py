a813b92de4db045eac1f1469ead2c1073627eb37
icarus <cesardedios94@gmail.com>
2023-05-08 20:52:43 -0400

    added while loop and exit

--- basc-calc/calc.py
+++ basc-calc/calc.py
@@ -1,17 +1,21 @@
-# User input two numbers and an operator
-print("Welcome to the calculator program!")
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-operator = input("Enter operator: ")
+operator = ""
 
-# Perform calculation based on operator
-if operator == "+":
-    print(f"The result is {num1 + num2}")
-elif operator == "-":
-    print(f"The result is {num1 - num2}")
-elif operator == "*":
-    print(f"The result is {num1 * num2}")
-elif operator == "/":
-    print(f"The result is {num1 / num2}")
-else:
-    print("Invalid operator entered. Please choose from +, -, *, /")
+while operator != "exit":
+    # User input two numbers and an operator
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    operator = input("Enter operator: ")
+
+    # Perform calculation based on operator
+    if operator == "+":
+        print(f"The result is: {num1 + num2}\n")
+    elif operator == "-":
+        print(f"The result is: {num1 - num2}\n")
+    elif operator == "*":
+        print(f"The result is: {num1 * num2}\n")
+    elif operator == "/":
+        print(f"The result is: {num1 / num2}\n")
+    elif operator == "exit":
+        print("Goodbye!")
+    else:
+        print("Invalid operator entered. Please choose from +, -, *, /, exit")
